f45efd93e6032776a9b85051551fbb35556a2243
Bruno Gabriel <tuba@Brunos-Mac-Pro.local>
2021-06-29 12:11:27 -0300

    HomeScreen and CarComponent

---
+++ chapter-3/rentx/src/screens/Home/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+
+import { Car } from '../../components/Car';
+
+import {
+  Container,
+  Header,
+  HeaderContent,
+  TotalCars,
+  CarList
+} from './styles';
+
+
+export function Home(){
+  //from Car component Props
+  const carData = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120
+    },
+    thumbnail: 'https://beta.alpes.one/storage/app/uploads/public/5f1/58b/578/5f158b57828a1446732254.png'
+  }
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo 
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <CarList 
+        data={[1,2,3,4,5,6,7]}
+        keyExtractor={item => String(item)}
+        renderItem={({ item }) => <Car data={carData}/>}
+      />
+    </Container>
+  );
+}
