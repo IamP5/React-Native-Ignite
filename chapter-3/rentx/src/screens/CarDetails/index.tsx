c5e63453345b4869d179aa80e4812f0d504c5d05
Bruno Gabriel <tuba@Brunos-Mac-Pro.local>
2021-07-01 16:37:38 -0300

    Acessory Component

--- chapter-3/rentx/src/screens/CarDetails/index.tsx
+++ chapter-3/rentx/src/screens/CarDetails/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
+
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Acessory } from '../../components/Acessory';
+
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
+
+
 
 import {
   Container,
@@ -15,6 +26,7 @@ import {
   Period,
   Price,
   About,
+  Acessories,
 
 } from './styles';
 
@@ -42,9 +54,18 @@ export function CarDetails(){
           </Rent>
         </Details>
 
+        <Acessories>
+          <Acessory name="380Km/h" icon={SpeedSvg}/>
+          <Acessory name="3.2s" icon={AccelerationSvg}/>
+          <Acessory name="800 HP" icon={ForceSvg}/>
+          <Acessory name="Gasolina" icon={GasolineSvg}/>
+          <Acessory name="Auto" icon={ExchangeSvg}/>
+          <Acessory name="2 pessoas" icon={PeopleSvg}/>
+        </Acessories>
+
         <About>
           Este é um automóvel desportivo. Surgiu do lendário
-          touro de lde indultado na praça Real Maestranza de Sevilla.
+          touro de lide indultado na praça Real Maestranza de Sevilla.
           É um belíssimo carro para quem gosta de acelerar.
         </About>
 
