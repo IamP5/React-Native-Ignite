83ea30a0b3b3cfba2532a71f30be940de87acd7e
Bruno Gabriel <tuba@Brunos-Mac-Pro.local>
2021-07-01 14:04:54 -0300

    Car Detail Screen

--- chapter-3/rentx/src/screens/CarDetails/index.tsx
+++ chapter-3/rentx/src/screens/CarDetails/index.tsx
@@ -5,7 +5,17 @@ import { ImageSlider } from '../../components/ImageSlider';
 import {
   Container,
   Header,
-  CarImages
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+
 } from './styles';
 
 export function CarDetails(){
@@ -18,6 +28,28 @@ export function CarDetails(){
       <CarImages>
         <ImageSlider imagesUrl={['https://beta.alpes.one/storage/app/uploads/public/5f1/58b/578/5f158b57828a1446732254.png']}/>
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é um automóvel desportivo. Surgiu do lendário
+          touro de lde indultado na praça Real Maestranza de Sevilla.
+          É um belíssimo carro para quem gosta de acelerar.
+        </About>
+
+      </Content>
+
     </Container>
   );
 }
