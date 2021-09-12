7bae724e1a8df576a1e80e175418e913cc68add6
Tuba <bdomini1@ford.com>
2021-09-11 21:05:02 -0300

    Splash Screen

--- chapter-3/rentx/src/routes/stack.routes.tsx
+++ chapter-3/rentx/src/routes/stack.routes.tsx
@@ -7,6 +7,7 @@ import { Scheduling } from '../screens/Scheduling';
 import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { SchedulingComplete } from '../screens/SchedulingComplete';
 import { MyCars } from '../screens/MyCars';
+import { Splash } from '../screens/Splash';
 
 
 
@@ -14,7 +15,11 @@ const { Navigator, Screen } = createStackNavigator();
 
 export function StackRoutes() {
   return(
-    <Navigator headerMode="none">
+    <Navigator headerMode="none" initialRouteName="Splash">
+      <Screen 
+        name="Splash"
+        component={Splash}
+      />
       <Screen 
         name="Home"
         component={Home}
